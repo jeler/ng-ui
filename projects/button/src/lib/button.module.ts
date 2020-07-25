cdada0e0177c5bbb60436dd6ff572d8ed6c004d4
jeler <jennifereler@MacBook-Pro.local>
2020-07-25 13:48:51 -0500

    style fun

--- projects/button/src/lib/button.module.ts
+++ projects/button/src/lib/button.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon"
 
 
 
 @NgModule({
   declarations: [ButtonComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   exports: [ButtonComponent]
 })
--- src/stories/Custon-Button.stories.ts
+++ src/stories/Custon-Button.stories.ts
@@ -1,11 +1,17 @@
 import { storiesOf } from "@storybook/angular";
 import { ButtonComponent } from "../../projects/button/src/public-api";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
 
 storiesOf('button', module)
   .add('basic', () => ({
     component: ButtonComponent,
     props: {
       text: 'hello'
+    },
+    moduleMetadata: {
+      imports: [MatButtonModule, MatDividerModule, MatIconModule]
     }
   }))
   .add('disabled', () => ({
@@ -13,5 +19,8 @@ storiesOf('button', module)
     props: {
       text: 'disabled',
       disabled: true
+    },
+    moduleMetadata: {
+      imports: [MatButtonModule, MatDividerModule, MatIconModule]
     }
   }));
